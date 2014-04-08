d0d5463e88a8b337e5838152dd9bc7237c58ad8f
kabernsj <kabernsj@gmail.com>
2014-04-08 16:42:01 -0500

    I hereby declare the following order: All orders shall have the required methods. Thus concludes this order pertaining to orders.

--- RestaurantManagementSystem/src/interfaces/OrderInterface.java
+++ RestaurantManagementSystem/src/interfaces/OrderInterface.java
@@ -1,5 +1,13 @@
 package interfaces;
 
 public interface OrderInterface {
-
+    void modifyDrink(String newValue);
+    void modifyAppetizer(String newValue);
+    void modifyMeal(String newValue);
+    void modifySide(String newValue);
+    void modifySpecial(String newValue);
+    void modifyStatus(String newValue);
+    int getID();
+    long getTimestamp();
+    String getStatus();
 }
